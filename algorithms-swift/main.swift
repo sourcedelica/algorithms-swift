c18cfa9db99222f71fe8d4acfdeec23a1d8e2e79
Eric Pederson <ericacm@gmail.com>
2015-05-25 11:15:13 -0400

    Add main.swift

---
+++ algorithms-swift/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  algorithms-swift
+//
+//  Created by Eric Pederson on 5/24/15.
+//  Copyright (c) 2015 Sourcedelica. All rights reserved.
+//
+
+import Foundation
+
+
+println("Hello, World!")
+
