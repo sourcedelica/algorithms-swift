397696dde1a0c0ae7fd454839015f70961c5b4b1
Eric Pederson <ericacm@gmail.com>
2015-05-28 22:47:05 -0400

    Add comments

--- algorithms-swift/Pulverizer.swift
+++ algorithms-swift/Pulverizer.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 class Pulverizer {
-    // Returns (gcd(a, b), s, t) such that gcd(a, b) = sa + tb
-    // http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+    /**
+        Computes (gcd(a, b), s, t) such that gcd(a, b) = sa + tb
+        http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
 
-    // TODO
-    // The quotients of a and b by their greatest common divisor, which are output,
-    // may have an incorrect sign. This is easy to correct at the end of the computation,
-    // but has not been done here for simplifying the code.
-    // Similarly, if either a or b is zero and the other is negative,
-    // the greatest common divisor that is output is negative,
-    // and all the signs of the output must be changed.
+        The quotients of a and b by their greatest common divisor, which are output,
+        may have an incorrect sign.  If this is the case, correct by
+        s -= b, t += a.
+
+        Similarly, if either a or b is zero and the other is negative,
+        the greatest common divisor that is output is negative,
+        and all the signs of the output must be changed.
+    */
     func extendedGCD(var a: Int, var b: Int) -> (Int, Int, Int) {
         var s = 0; var old_s = 1
         var t = 1; var old_t = 0
--- algorithms-swift/main.swift
+++ algorithms-swift/main.swift
@@ -1,9 +1,17 @@
 import Foundation
 
-println(Pulverizer().extendedGCD(259, b: 70))
-println(Pulverizer().extendedGCD(70, b: 259))
-println(Pulverizer().extendedGCD(6, b: 3))
-println(Pulverizer().extendedGCD(3, b: 6))
-println(Pulverizer().extendedGCD(0, b: 5))
-println(Pulverizer().extendedGCD(5, b: 0))
+let P = Pulverizer()
+let a = 50
+let b = 21
+let (g, s, t) = P.extendedGCD(50, b: 21)
+let sp = s - b
+let tp = t + a
+let gp = sp * a + tp * b
+println("a=\(a) b=\(b) g=\(g) s=\(s) t=\(t) sp=\(sp) tp=\(tp) gp=\(gp)")
+println(P.extendedGCD(259, b: 70))
+println(P.extendedGCD(70, b: 259))
+println(P.extendedGCD(6, b: 3))
+println(P.extendedGCD(3, b: 6))
+println(P.extendedGCD(0, b: 5))
+println(P.extendedGCD(5, b: 0))
 
